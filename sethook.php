7fbae8a04f60a1b684d5ce9ec531a30e227f1737
Rafael Delboni <rafadelboni@gmail.com>
2016-04-10 17:49:27 -0300

    Rowback new telgram-bot-api version

--- sethook.php
+++ sethook.php
@@ -2,19 +2,19 @@
 //Composer Loader
 $loader = require __DIR__.'/vendor/autoload.php';
 
-$API_KEY = 'YOU_API_key';
+$API_KEY = '123456789:your_bot_api_key';
 // botname cann't contain with '@'
-$BOT_NAME = 'you_boot_name';
-$hook_url = 'https://www.domain.com/path/webhook.php';
+$BOT_NAME = 'your_bot_name';
+$hook_url = 'https://you.domain.com/path/webhook.php';
 // you should put certificate under ./cert folder 
 // the server.crt should sign the nginx as same
 //
-$certificate_file = __DIR__.'/cert/server.crt';
+//$certificate_file = __DIR__.'/cert/server.crt';
 try {
     // create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
     // set webhook
-    $result = $telegram->setWebHook($hook_url, $certificate_file);
+    $result = $telegram->setWebHook($hook_url);
     if ($result->isOk()) {
         echo $result->getDescription();
     }
